50db0a9e6246262875145e96c5461075e887ee44
timmy_3000 <66499629+timmy3000@users.noreply.github.com>
2022-05-27 09:57:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,23 +15,42 @@ def read_file_content(filename):
     return text
 
 
+def indexOf(arr, delimiter):
+
+    for i in range(len(arr)):
+        if arr[i] == delimiter:
+            return i
+
+    return -1
+
+
 def count_words():
     text = read_file_content("./story.txt")
     # [assignment] Add your code here
     
     for character in text:
         if not(character.isalpha() or character.isnumeric() or character.isspace()):
-            text = text.replace(character, "")
+            text = text.replace(character, "").lower()
 
 
     text = text.split()
 
-    word_count = []
+    text_dictionary = {}
+    count = 0
 
-    for word in text:
-        word_count.append(len(word))
+    find = ""
 
-    text_dictionary = dict(zip(text, word_count))
+    while len(text) > 0:
+        find = text[0]
+
+        while indexOf(text, find) != -1:
+            count += 1
+            del text[indexOf(text, find)]
+
+
+        text_dictionary[find] = count
+        count = 0
+
 
     return text_dictionary
 
